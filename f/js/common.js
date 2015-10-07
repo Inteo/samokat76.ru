57f95f4433978a4ea00758fa65f658c8ba1413ce
BerTepesh <Ber.Tepesh@gmail.com>
2015-10-07 15:05:16 +0300

    Гостевая

--- f/js/common.js
+++ f/js/common.js
@@ -4,5 +4,24 @@ $(function(){
   })
 });
 $(document).ready(function(){
-
+  $(".modal-inline").fancybox({
+    type: 'inline',
+    fixed:false,
+    title: '',
+    width: '100%',
+    height: 'auto',
+    padding: 0,
+    autoResize: false,
+    autoSize: false,
+    maxWidth: 620,
+    autoCenter: false,
+    fitToView: false,
+    minWidth: 280,
+    scrolling   : 'no',
+    helpers: {
+      overlay: {
+        fixed: false
+      }
+    }
+  });
 });
